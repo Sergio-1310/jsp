db80a0b042ccc36e934bb5a7bfc8ce8f21021deb
Sergio-1310 <amayasergio1310@gmail.com>
2026-05-18 14:40:33 -0500

    conexion db

--- src/java/conexion/Conexion.java
+++ src/java/conexion/Conexion.java
@@ -1,37 +1,25 @@
 package conexion;
-
 import java.sql.Connection;
 import java.sql.DriverManager;
 
 public class Conexion {
-
     Connection con;
-
     public Connection conectar() {
-
         try {
-
             Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
-
-            String url = "jdbc:sqlserver://DESKTOP-83I5SAH\\SQLEXPRESS;"
+            String url = "jdbc:sqlserver://localhost:63767;"
                     + "databaseName=dbEcommerce1;"
                     + "encrypt=true;"
                     + "trustServerCertificate=true;"
-                    + "integratedSecurity=true;";
-
+                    + "user=usuario1;"
+                    + "password=123;";
             System.out.println("Intentando conectar a SQL Server...");
-
             con = DriverManager.getConnection(url);
-
             System.out.println("Conexion exitosa");
-
         } catch (Exception e) {
-
             System.out.println("Error conexion:");
             e.printStackTrace();
-
         }
-
         return con;
     }
 }
